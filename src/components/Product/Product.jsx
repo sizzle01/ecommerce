df5de95e5aa1dd5200bc08060949aa02a03fe6ce
Your Name <you@example.com>
2021-03-05 20:47:54 +0100

    product card setup

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+import useStyles from "./styles";
+
+const Product = ({ product }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardContent>
+        <div className={classes.CardContent}>
+          <Typography variant="h5">{product.name}</Typography>
+          <Typography variant="h6">{product.description}</Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton arial-label="Add to cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import Grid from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import Product from "../Product/Product";
 
 const products = [
-  { id: 1, name: "shoes", description: "running shoes." },
-  { id: 2, name: "macbook", description: "Apple macbook." },
+  { id: 1, name: "shoes", description: "running shoes.", price: "$5" },
+  { id: 2, name: "macbook", description: "Apple macbook.", price: "$10" },
 ];
 
 const Products = () => {
-  <main>
-    <Grid container justify="center" spacing={4}>
-      {produts.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <Product />
-        </Grid>
-      ))}
-    </Grid>
-  </main>;
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 };
 
 export default Products;
