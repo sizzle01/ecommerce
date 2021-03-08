36b331850260da472358f5a19a0eacf2d380f7ff
Your Name <you@example.com>
2021-03-08 19:27:51 +0100

    adding products to commercejs

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -11,24 +11,33 @@ import { AddShoppingCart } from "@material-ui/icons";
 
 import useStyles from "./styles";
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
+
   return (
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={product.image}
+        image={product.media.source}
         title={product.name}
       />
       <CardContent>
         <div className={classes.CardContent}>
           <Typography variant="h5">{product.name}</Typography>
-          <Typography variant="h5">{product.price}</Typography>
-          <Typography variant="body2">{product.description}</Typography>
+          <Typography variant="h5">
+            {product.price.formatted_with_symbol}
+          </Typography>
+          <Typography
+            dangerouslySetInnerHTML={{ __html: product.description }}
+            variant="body2"
+          />
         </div>
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton arial-label="Add to cart">
+        <IconButton
+          arial-label="Add to cart"
+          onClick={() => onAddToCart(product.id, 1)}
+        >
           <AddShoppingCart />
         </IconButton>
       </CardActions>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,24 +2,8 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "../Product/Product";
 import useStyles from "./styles";
-const products = [
-  {
-    id: 1,
-    name: "shoes",
-    description: "running shoes.",
-    price: "$5",
-    image: "https://loremflickr.com/320/240",
-  },
-  {
-    id: 2,
-    name: "macbook",
-    description: "Apple macbook.",
-    price: "$10",
-    image: "https://loremflickr.com/320/240",
-  },
-];
 
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
@@ -27,7 +11,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
